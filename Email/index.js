d098b371fbf3ae34f274bf0e55e23926be35ddf2
wangjl0613 <64774766+wangjl0613@users.noreply.github.com>
2023-10-11 16:47:49 +0800

    v1

---
+++ Email/index.js
@@ -0,0 +1,28 @@
+emailjs.init("GIhZPlnyyTE5PSumz");
+
+const sendBtn = document.querySelector('.send-btn');
+const result = document.querySelector('.result');
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail(){
+    //Get the form data
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    //Send the email using EmailJS
+    emailjs.send("service_gxr13qc", "template_rj5futq", {
+        to_email: to ,
+        subject: subject,
+        message: message
+    }).then(function() {
+        result.innerHTML = "Email sent successfully!";
+        result.style.opacity = 1;
+    }, function(error){
+        result.innerHTML = "Email sending failed!!";
+        result.style.opacity = 1;
+        console.log(error); 
+
+    });
+}
